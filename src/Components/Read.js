c40732c4b4439169eb934e7cce278115dddee4bb
reema-2 <reeemaa23@gmail.com>
2020-03-27 16:57:48 +0300

    updated reed.js

--- src/Components/Read.js
+++ src/Components/Read.js
@@ -22,7 +22,7 @@ class Read extends Component{
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${books.imageLinks.thumbnail})` }}></div>
                             <div className="book-shelf-changer">
                             <select value={books.shelf} onChange={(event) => this.props.onItemSelect(books, event.target.value)}>
-                                <option value="move">Move to...</option>
+                                <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
